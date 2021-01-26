5a152d47fb49f529dbcdb3091460501267e5b193
kanokkorn <kanokorn@outlook.jp>
2021-01-26 21:23:38 +0700

    python stuff

---
+++ scripts/server.py
@@ -0,0 +1,9 @@
+import http.server
+import socketserver
+
+port = 39393
+
+Handler = http.server.SimpleHTTPRequestHandler
+with socketserver.TCPServer(('', port), Handler) as httpd:
+  print('serving at port', port)
+  httpd.serve_forever()
