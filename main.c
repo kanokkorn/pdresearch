a07bd7fa17bc76fb991dcc321fa7b2e21795f637
kanokkorn <kanokorn@outlook.jp>
2021-03-21 21:45:33 +0700

    rename, add readme.md

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 /* HTTP GET client with 5 timeout written in C99 */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -8,22 +9,21 @@ int main(void) {
   CURL *curl;
   CURLcode res;
   int timeout = 0;
-  while(1){
+  while(timeout != 3){
     curl = curl_easy_init();
     if(curl) {
       curl_easy_setopt(curl, CURLOPT_URL, "localhost:39393");
       curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
       res = curl_easy_perform(curl);
         if (res != CURLE_OK) {
-          fprintf(stderr, "failed: %s for %d times\n",
+          fprintf(stderr, "Failed: %s for %d times\n",
               curl_easy_strerror(res), timeout+1);
-          timeout == 4 ? exit(EXIT_FAILURE) : timeout++;
-        } else {
+          timeout++;
+        } else
           timeout = 0;
         }
-      curl_easy_cleanup(curl);
-    } sleep(1);
+    curl_easy_cleanup(curl);
+    sleep(3);
   }
   return 0;
 }
-
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Platform development research
+
+## Client 
+
+```bash
+./configure
+make -j
+```
+
+## Mock server
+
+```bash
+sudo pip3 install -r requirements.txt
+python3 scripts/server.py
+```
